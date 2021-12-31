473629021ef477a7e4a242042c8b031b1ff86151
Benjamin Orimoloye <bjfikky@yahoo.com>
2021-12-31 01:37:01 -0500

    Initial Commit

---
+++ Trivvy/TrivvyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrivvyApp.swift
+//  Trivvy
+//
+//  Created by Benjamin Orimoloye on 12/31/21.
+//
+
+import SwiftUI
+
+@main
+struct TrivvyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
